f91c21d7ee1c11b9c8d2e8947699a54a8cdebb46
fbubeck <ferdinand.bubeck@gmx.de>
2021-09-24 09:54:42 +0200

    new

--- test.py
+++ test.py
@@ -4,14 +4,21 @@ from tensorflow import keras
 
 model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 
-
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-
+# Train Data
 xs = [1, 2, 3]
 ys = [2, 4, 6]
 
-model.fit(xs, ys, epochs=1000)
-print(model.predict([7]))
+# Test Data
+xs_test = [7, 8, 9]
 
-# test
+# Callback für TensorBoard
+tensorboard_callback = tf.keras.callbacks.TensorBoard(
+    log_dir="/logs{}", histogram_freq=1)
+
+# Modelfitting
+model.fit(xs, ys, epochs=1000, callbacks=[tensorboard_callback])
+
+# Print Results
+print(model.predict(xs_test))
