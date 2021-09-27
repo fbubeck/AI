c620cdf6c5489d9af8ec735a9d9a60fd24e7da01
fbubeck <ferdinand.bubeck@gmx.de>
2021-09-27 10:14:54 +0200

    added MSE

--- linearRegression.py
+++ linearRegression.py
@@ -6,26 +6,32 @@ from time import time
 start = time()
 
 # Train Data
-xs = [1, 2, 3]
-ys = [2, 4, 6]
+xs_train = [1, 2, 3]
+ys_train = [2, 4, 6]
 
 # Test Data
 xs_test = [7, 8, 9]
+ys_test = [14, 16, 18]
 
 # reshape Arrays
-xs = np.matrix(xs).T.A
-ys = np.matrix(ys).T.A
+xs_train = np.matrix(xs_train).T.A
+ys_train = np.matrix(ys_train).T.A
 xs_test = np.matrix(xs_test).T.A
+ys_test = np.matrix(ys_test).T.A
 
 # Modelfitting
 model = linear_model.LinearRegression()
-model.fit(xs, ys)
+model.fit(xs_train, ys_train)
 
 # Predictions
 y_pred = model.predict(xs_test)
 
 print("Predictions for inputs 7, 8, 9: ")
 print(y_pred)
+
+# MSE
+print("Mean squared error: %.2f" %
+      np.mean((y_pred - ys_test) ** 2))
 
 end = time()
 print(f'It took {end - start} seconds!')
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
 from time import time
 
 start = time()
@@ -9,21 +10,28 @@ model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 # Train Data
-xs = [1, 2, 3]
-ys = [2, 4, 6]
+xs_train = [1, 2, 3]
+ys_train = [2, 4, 6]
 
 # Test Data
 xs_test = [7, 8, 9]
+ys_test = [14, 16, 18]
 
 # Callback für TensorBoard
 tensorboard_callback = tf.keras.callbacks.TensorBoard(
     log_dir="/logs{}", histogram_freq=1)
 
 # Modelfitting
-model.fit(xs, ys, epochs=1000, callbacks=[tensorboard_callback])
+model.fit(xs_train, ys_train, epochs=1000, callbacks=[tensorboard_callback])
 
-# Print Results
-print(model.predict(xs_test))
+# Predictions
+y_pred = model.predict(xs_test)
+print("Predictions for inputs 7, 8, 9: ")
+print(y_pred)
+
+# MSE
+print("Mean squared error: %.2f" %
+      np.mean((y_pred - ys_test) ** 2))
 
 end = time()
 print(f'It took {end - start} seconds!')
